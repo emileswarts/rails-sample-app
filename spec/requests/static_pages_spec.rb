3dab656459a3e3239b013702667cf6b85422eb00
Emile <emile.swarts123@gmail.com>
2012-05-19 21:45:52 +0100

    use variable for duplicated content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App "}
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
@@ -9,7 +10,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title',
-									 :text => "Ruby on Rails Tutorial Sample App | Home")
+									 :text => "#{base_title}| Home")
 		end
 	end
 
@@ -21,7 +22,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title',
-									 :text => 'Ruby on Rails Tutorial Sample App | Help')
+									 :text => "#{base_title}| Help")
 		end
 	end
 
@@ -33,7 +34,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title',
-									 :text => 'Ruby on Rails Tutorial Sample App | About')
+									 :text => "#{base_title}| About")
 		end
 	end
 
