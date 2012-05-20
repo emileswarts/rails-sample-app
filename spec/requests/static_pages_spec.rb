1104c2068a26be009b72ba4c58dadaf18d5a464d
Emile <emile.swarts123@gmail.com>
2012-05-20 13:38:48 +0100

    named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,27 +6,27 @@ describe "Static Pages" do
 
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title',
 									 :text => "#{base_title}")
 		end
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help'" do
-          	visit '/static_pages/help'
+          	visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title',
 									 :text => "#{base_title} | Help")
 		end
@@ -34,25 +34,25 @@ describe "Static Pages" do
 
 	describe "About page" do
 		it "should have the content 'About'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About')
 		end
 		it "should have the right title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title',
 									 :text => "#{base_title} | About")
 		end
 	end
 
 	describe "Contact page" do
-		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
+		it "should have the h1 'Contact'" do
+			visit contact_path
 			page.should have_selector('h1', :text => 'Contact')
 		end
 		it "should have the right title" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title',
-									 :text => 'Ruby on Rails Tutorial Sample App | Contact')
+									 :text => "#{base_title} | Contact")
 		end
 	end
 end
