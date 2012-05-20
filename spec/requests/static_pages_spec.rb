ab09a061cfc01daeab0b1dd248676f6d3dc46d1a
Emile <emile.swarts123@gmail.com>
2012-05-20 15:50:15 +0100

    add test for clicking from template

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,5 +26,20 @@ describe "Static Pages" do
 		it { should have_selector('h1', :text => 'Contact') }
 		it { should have_selector('title', :text => full_title('Contact')) }
 	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About')
+		click_link "Help"
+		page.should have_selector 'title', text: full_title('Help')
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+		click_link "Home"
+		click_link "Sign up now!"
+		page.should have_selector 'title', text: full_title('Sign up')
+		click_link "sample app"
+		page.should have_selector 'title', text: full_title('')
+	end
 end
 
