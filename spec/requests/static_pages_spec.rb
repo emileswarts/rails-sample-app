a1c80a619ee8354f62d672b2ae7c3f36ce32922f
Emile <emile.swarts123@gmail.com>
2012-05-20 14:46:41 +0100

    use before instead of defining visit path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,52 +5,47 @@ describe "Static Pages" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
 	describe "Home page" do
+		before { visit root_path }
 		it "should have the content 'Sample App'" do
-			visit root_path
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 		it "should have the base title" do
-			visit root_path
 			page.should have_selector('title',
 									 :text => "#{base_title}")
 		end
 		it "should not have a custom page title" do
-			visit root_path
 			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 
 	describe "Help page" do
+		before { visit help_path }
 		it "should have the content 'Help'" do
-          	visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the right title" do
-			visit help_path
 			page.should have_selector('title',
 									 :text => "#{base_title} | Help")
 		end
 	end
 
 	describe "About page" do
+		before { visit about_path }
 		it "should have the content 'About'" do
-			visit about_path
 			page.should have_selector('h1', :text => 'About')
 		end
 		it "should have the right title" do
-			visit about_path
 			page.should have_selector('title',
 									 :text => "#{base_title} | About")
 		end
 	end
 
 	describe "Contact page" do
+		before { visit contact_path }
 		it "should have the h1 'Contact'" do
-			visit contact_path
 			page.should have_selector('h1', :text => 'Contact')
 		end
 		it "should have the right title" do
-			visit contact_path
 			page.should have_selector('title',
 									 :text => "#{base_title} | Contact")
 		end
