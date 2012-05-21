71e6b0bc77cbb46a6b5274a0fae6d43a2a70a323
Emile <emile.swarts123@gmail.com>
2012-05-21 21:53:37 +0100

    add tests for user validation and model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,20 @@ describe User do
 
 	it { should respond_to(:name)}
 	it { should respond_to(:email)}
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before {@user.name = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid }
+	end
 end
