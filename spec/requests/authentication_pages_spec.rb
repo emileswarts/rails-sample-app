2bfd44977804ead578d5e316f6563b432abc96c9
Emile <emile.swarts123@gmail.com>
2012-06-10 15:07:02 +0100

    Basic authorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,24 @@ describe "Authentication" do
 
 	subject { page }
 
+	describe "authorization" do
+		describe "for non signed in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in the users controller" do
+				describe "visiting the edit page" do
+					before {visit edit_user_path(user)}
+					it {should have_selector('title', text: "Sign in")}
+				end
+
+				describe "submitting to the update action" do
+					before { put user_path(user) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+		end
+	end
+
 	describe "signin" do
 
 		before { visit signin_path }
